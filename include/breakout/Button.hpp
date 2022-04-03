dca94961495bcb1725c19e25576a157b917d65b3
matinraayai <raayaiardakani.m@husky.neu.edu>
2022-04-03 02:52:38 -0400

    Added revamped button.

--- include/breakout/Button.hpp
+++ include/breakout/Button.hpp
@@ -1,87 +1,22 @@
 #ifndef BUTTON_HPP
 #define BUTTON_HPP
-
-#include "Mouse.hpp"
+#include <functional>
+#include <base/GameObject.hpp>
+#include "base/ResourceManager.hpp"
+#include "base/TextObject.hpp"
+#include "base/Level.hpp"
+#include "base/SelectableComponent.hpp"
 
 /**
- * @brief A class representing a clickable UI Text Button in our game.
+ * @brief A class representing a clickable UI Text Button.
  */
-class Button {
+class Button : public GameObject {
  private:
-  SDL_Texture *texture_;
-  std::string text_;
-  SDL_Texture *textTexture;
-  SDL_Rect Src;
-  SDL_Rect Dest;
-  SDL_Rect Text_Dest;
-  int originalYPos;
-  int clickFactor;
-  bool isSelected = false;
+  std::shared_ptr<TextObject> mText;
 
  public:
-  /**
-   * @brief Construct an empty Button object, don't forget to call startUp().
-   */
-  Button();
-
-  /**
-   * @brief Destroy the Button object, don't forget to call shutDown().
-   */
-  virtual ~Button();
-
-  /**
-   * @brief Initializes/Configures this Button for use and display.
-   *
-   * @param srcw Source width of texture to render
-   * @param srch Source height of texture to render
-   * @param srcx Source x coord of texture to render
-   * @param srcy Source y coord of texture to render
-   * @param destw Destination width of texture to render
-   * @param desth Destination height of texture to render
-   * @param destx Destination x coord of texture to render
-   * @param desty Destination y coord of texture to render
-   * @param text String to render onto the Button
-   */
-  void startUp(int srcw, int srch, int srcx, int srcy, int destw, int desth,
-               int destx, int desty, std::string text);
-
-  /**
-   * @brief Cleans up this Button.
-   */
-  void shutDown();
-
-  /**
-   * @brief Mouse click handler, e.g. plays sound.
-   */
-  void isClicked();
-
-  /**
-   * @brief Whether or not this Button is selected.
-   *
-   * @return true/false
-   */
-  bool IsSelected() const;
-
-  /**
-   * @brief Updates depiction of this Button on screen, based on mouse position.
-   *
-   * @param mouse (in) Mouse object to use to get its position
-   */
-  void update(Mouse *mouse);
-
-  /**
-   * @brief Calls SDL Render to show Button on screen.
-   *
-   * @param renderer (in) an external SDL renderer to use
-   */
-  void render(SDL_Renderer *renderer);
-
-  /**
-   * @brief Change the language of the text on this Button.
-   *
-   * @param language The desired language
-   */
-  void changeLanguage(Language language);
+  Button(Level& level, float x, float y, float w, float h, const std::string& text,
+         const std::function<void(void)>& selectHook);
 };
 
 #endif
--- src/breakout/Button.cpp
+++ src/breakout/Button.cpp
@@ -1,75 +1,15 @@
-#include "Button.hpp"
+#include <breakout/Tag.hpp>
+#include "breakout/Button.hpp"
+#include "base/TextObject.hpp"
+#include "base/TextureRenderComponent.hpp"
+#include "base/InputManager.hpp"
 
-Button::Button() {}
-
-Button::~Button() {}
-
-void Button::startUp(int srcw, int srch, int srcx, int srcy, int destw,
-                     int desth, int destx, int desty, std::string text) {
-  static SDL_Texture *t =
-      ResourceManager::getInstance().getTexture("buttonSpriteSheet.png");
-  texture_ = t;
-  text_ = text;
-
-  textTexture = ResourceManager::getInstance().getTextTexture(
-      text, "Gageda.ttf", {255, 255, 255, 0}, 64);
-
-  Src.w = srcw;
-  Src.h = srch;
-  Src.x = srcx;
-  Src.y = srcy;
-
-  int w, h;
-  SDL_QueryTexture(textTexture, nullptr, nullptr, &w, &h);
-  Text_Dest.w = w;
-  Text_Dest.h = h;
-
-  Dest.w = Text_Dest.w + 70;
-  Dest.h = desth;
-  Dest.x = destx - (Dest.w / 2);
-  Dest.y = desty;
-
-  Text_Dest.x = Dest.x + 30;
-  Text_Dest.y = Dest.y + 30;
-
-  originalYPos = Text_Dest.y;
-  clickFactor = Text_Dest.y + 3;
-}
-
-void Button::shutDown() {}
-
-void Button::isClicked() {
-  isSelected = false;
-
-  // Button Click sound.
-  Mix_PlayChannel(
-      1, ResourceManager::getInstance().getChunk("ButtonClick_SFX.wav"), 0);
-}
-
-void Button::update(Mouse *mouse) {
-  if (SDL_HasIntersection(&Dest, &mouse->point)) {
-    isSelected = true;
-    // TODO: get rid of Hard codes
-    Src.x = 57;
-    Text_Dest.y = clickFactor;
-  } else {
-    isSelected = false;
-    // TODO: get rid of Hard codes
-    Src.x = 635;
-    Text_Dest.y = originalYPos;
-  }
-}
-
-void Button::render(SDL_Renderer *renderer) {
-  SDL_RenderCopy(renderer, texture_, &Src, &Dest);
-  SDL_RenderCopy(renderer, textTexture, NULL, &Text_Dest);
-}
-
-bool Button::IsSelected() const {
-  return isSelected;
-}
-void Button::changeLanguage(Language language) {
-  textTexture = ResourceManager::getInstance().getTextTexture(
-      ResourceManager::getInstance().getTranslation(text_, language),
-      "Gageda.ttf", {255, 255, 255, 0}, 64);
+Button::Button(Level& level, float x, float y, float w, float h, const std::string& text,
+               const std::function<void(void)>& selectHook) : GameObject(level, x, y, w, h, ButtonTag),
+               mText(std::make_shared<TextObject>(level, x + 30, y + 30, 64, text)){
+  auto renderComponent = std::make_shared<TextureRenderComponent>(*this);
+  renderComponent->setTexture(ResourceManager::getInstance().getTexture("buttonSpriteSheet.png"));
+  level.addObject(mText);
+  auto selectableComponent = std::make_shared<SelectableComponent>(*this, selectHook);
+  addGenericComponent(selectableComponent);
 }
