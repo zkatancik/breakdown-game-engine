a1233e9f5b913defc717b94ed488b44a306704f2
zkatancik <katancik.z@husky.neu.edu>
2022-04-05 02:01:29 -0400

    text centering again

--- src/breakout/Button.cpp
+++ src/breakout/Button.cpp
@@ -1,12 +1,17 @@
-#include <breakout/Tag.hpp>
 #include "breakout/Button.hpp"
+
+#include <breakout/Tag.hpp>
+
 #include "base/InputManager.hpp"
 
-Button::Button(Level& level, float x, float y, float w, float h, Color color, const std::string& text,
-               const std::function<void(void)>& selectHook) : GameObject(level, x, y, w, h, ButtonTag) {
+Button::Button(Level& level, float x, float y, float w, float h, Color color,
+               const std::string& text,
+               const std::function<void(void)>& selectHook)
+    : GameObject(level, x, y, w, h, ButtonTag) {
   // Load sprite sheet for the buttons
   buttonRenderer = std::make_shared<TextureRenderComponent>(*this);
-  buttonRenderer->setTexture(ResourceManager::getInstance().getTexture("2DBreakout/Graphics/buttonSpriteSheet.png"));
+  buttonRenderer->setTexture(ResourceManager::getInstance().getTexture(
+      "2DBreakout/Graphics/buttonSpriteSheet.png"));
   // Set crops from the sprite sheet.
   switch (color) {
     case RED:
@@ -20,20 +25,28 @@ Button::Button(Level& level, float x, float y, float w, float h, Color color, co
       break;
   }
   // Set event handlers for the SelectableComponent
-  auto mHoverChangeFocus = [&] () {buttonRenderer->setCrop(mCropFocused);};
-  auto mNotSelectChangeFocus = [&] () {buttonRenderer->setCrop(mCropNotFocused);};
+  auto mHoverChangeFocus = [&]() { buttonRenderer->setCrop(mCropFocused); };
+  auto mNotSelectChangeFocus = [&]() {
+    buttonRenderer->setCrop(mCropNotFocused);
+  };
 
+  textRenderer = std::make_shared<TextureRenderComponent>(
+      *this, std::vector({buttonRenderer}));
 
-  textRenderer = std::make_shared<TextureRenderComponent>(*this, std::vector({buttonRenderer}));
-  textRenderer->setOffSetX(int(w / 4));
-  textRenderer->setOffSetY(int(h / 4));
   textRenderer->setRenderMode(TextureRenderComponent::RenderMode::QUERY);
 
   setRenderComponent(textRenderer);
 
-  textComponent = std::make_shared<TextComponent>(*this, text, mFontSize, mButtonFont, textRenderer);
+  textComponent = std::make_shared<TextComponent>(*this, text, mFontSize,
+                                                  mButtonFont, textRenderer);
   addGenericComponent(textComponent);
 
-  auto selectableComponent = std::make_shared<SelectableComponent>(*this, selectHook, mHoverChangeFocus, mNotSelectChangeFocus);
+  int textW, textH;
+  SDL_QueryTexture(textRenderer->GetTexture(), NULL, NULL, &textW, &textH);
+  textRenderer->setOffSetX(int((w - textW) / 2));
+  textRenderer->setOffSetY(int(30));
+
+  auto selectableComponent = std::make_shared<SelectableComponent>(
+      *this, selectHook, mHoverChangeFocus, mNotSelectChangeFocus);
   addGenericComponent(selectableComponent);
 }
