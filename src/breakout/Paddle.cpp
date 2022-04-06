f1d62ded04ce160747cbb83f65a9667f4498798c
matinraayai <raayaiardakani.m@husky.neu.edu>
2022-04-05 22:19:01 -0400

    Added ball/paddle collision sound.

--- src/breakout/Paddle.cpp
+++ src/breakout/Paddle.cpp
@@ -1,5 +1,5 @@
 #include "breakout/Paddle.hpp"
-#include "base/ResourceManager.hpp"
+
 
 
 Paddle::Paddle(Level& level, float sw, float sh, float width, float speed) : GameObject(level, sw / 2 - (width / 2), sh - 50, width, 24, PaddleTag) {
@@ -8,4 +8,8 @@ Paddle::Paddle(Level& level, float sw, float sh, float width, float speed) : Gam
   mRenderer->setTexture(ResourceManager::getInstance().getTexture("2DBreakout/Graphics/paddleRed.png"));
   setPhysicsComponent(std::make_shared<PhysicsComponent>(*this, b2_kinematicBody, false, 0.f, 10.f));
   addGenericComponent(std::make_shared<InputBreakoutComponent>(*this, speed));
+  addGenericComponent(
+      std::make_shared<PerformHookOnCollideComponent>(*this, BallTag, [&] (Level&, const std::shared_ptr<GameObject>&) {
+        Mix_PlayChannel(0, ResourceManager::getInstance().getChunk("2DBreakout/SFX/PaddleBorderHit_SFX.mp3"),0);
+      }));
 }
