3be17c72c07fa97d8b46592e0fe4bf7f099e0ce5
Marcin Kierzenka <33709159+mkierzenka@users.noreply.github.com>
2022-04-12 23:48:32 -0400

    Added Level data for TD game

--- include/custom/LevelData.hpp
+++ include/custom/LevelData.hpp
@@ -9,85 +9,45 @@
 /**
  * @brief The types of blocks possible.
  */
-enum class GraveRunnerBlockType {
-  // Colored Block
-  PlainBlock = 0,
-  // Hole or No block
-  NoBlock,
-  // Unbreakable Block
-  Wall,
-  // Key to unlock the exit
-  Key,
-  // Exit of the Level (Also the goal)
-  Exit
+enum class TdBlockType {
+  Plain = 0,
+  Path
 };
 
 /**
  * @brief The types of items present in a level.
  * - All the items you see in the LevelFile.
  */
-enum class GraveRunnerLevelItem {
+enum class TdLevelItem {
   NONE,
-  NOBLOCK = '.',
-  TILE1 = '1',
-  TILE2 = '2',
-  TILE3 = '3',
-  TILEX = 'X',
-  TILEY = 'Y',
-  TILEZ = 'Z',
-  BONES1,
-  BONES2,
-  BONES3,
-  ARROWSIGN,
-  BUSH1,
-  BUSH2,
-  CRATE,
-  DEADBUSH,
-  SIGN,
-  SKELETON,
-  TOMBSTONE1,
-  TOMEBSTONE2,
-  TREE,
-  JACK = 'P',
-  PATROLZOMBIE = 'E',
-  FOLLOWZOMBIE = 'F',
-  WALL = 'W',
-  KEY = 'K',
-  EXIT = 'G',
+  NOBLOCK,
+  PLAINBLOCK,
+  PATHBLOCK
 };
 
 /**
  * @brief Represents data of a single Block.
  * Can add color and more to be read from level file.
  */
-struct GraveRunnerBlockData {
-  GraveRunnerBlockType block_Type{GraveRunnerBlockType::PlainBlock};
+struct TdBlockData {
+  TdBlockType block_Type{TdBlockType::Plain};
   std::string blockNumber;
 };
 
 /**
  * @brief Has the data read from Level file.
  *
- * Level file data format:
- * row 1 - Row count
- * row 2 - Col Count
- * row 3 - Minimum number of Blocks to clear a level
- * row 4 onwards - List of Blocks, organized in rows
+ * TODO- write the file format here
  */
-struct GraveRunnerLevelData {
+struct TdLevelData {
   int levelNumber{0};
 
-  std::vector<std::vector<GraveRunnerLevelItem>> levelGrid;
+  std::vector<std::vector<TdLevelItem>> levelGrid;
 
-  std::vector<GraveRunnerBlockData> blocks;
+  std::vector<TdBlockData> blocks;
   int rowCount;
   int colCount;
   Vector2D<int> blockSize;
-  Vector2D<int> levelEndPosition{0, 0};
-  Vector2D<int> playerStartPosition{0, 0};
-
-  std::vector<Vector2D<int>> enemyStartPositions;
-  std::vector<Vector2D<int>> followerEnemyStartPositions;
 };
 
 /**
@@ -96,12 +56,14 @@ struct GraveRunnerLevelData {
  * @param blockDataList levelData loaded from the file
  * @param level level number
  */
-void loadLevel(GraveRunnerLevelData *levelData, int level);
+void loadLevel(TdLevelData *levelData, int levelNum);
 
-void updateCurrentLevel(GraveRunnerLevelData *levelData,
-                        Vector2D<int> gridPosition, GraveRunnerLevelItem item);
+/* TODO- add these back as needed
+void updateCurrentLevel(TdLevelData *levelData,
+                        Vector2D<int> gridPosition, TdLevelItem item);
 
-void updateLevelFile(GraveRunnerLevelData ld, Vector2D<int> gridPosition,
-                     GraveRunnerLevelItem item);
+void updateLevelFile(TdLevelData ld, Vector2D<int> gridPosition,
+                     TdLevelItem item);
+*/
 
 #endif
--- src/custom/LevelData.cpp
+++ src/custom/LevelData.cpp
@@ -6,143 +6,82 @@
 #include "base/ResPath.hpp"
 #include "base/ResourceManager.hpp"
 
-// void loadLevel(TdLevelData *levelData, int level) {
-//   levelData->levelGrid.clear();
-//   levelData->blocks.clear();
-//   levelData->enemyStartPositions.clear();
+void loadLevel(TdLevelData *levelData, int level) {
+  levelData->levelGrid.clear();
+  levelData->blocks.clear();
+  levelData->levelNumber = level;
 
-//   levelData->levelNumber = level;
+  const filesystem::path resPath = getResourcePath("TD2D/Levels");
+  std::string resourceFilename =
+      (resPath / ("level" + std::to_string(levelData->levelNumber) + ".txt"))
+          .string();
 
-//   const filesystem::path resPath = getResourcePath("Graverunner/levels");
-//   std::string resourceFilename =
-//       (resPath / ("level" + std::to_string(levelData->levelNumber) + ".txt"))
-//           .string();
+  std::string line;
+  std::fstream *myfile = ResourceManager::getInstance().openFile(
+      resourceFilename, std::ios_base::in);
+  if (myfile->is_open()) {
+    int lineCounter = 0;
+    int colCounter = 0;
+    std::string x;
+    std::string y;
+    while (std::getline(*myfile, line)) {
+      if (lineCounter == 0) {
+        levelData->rowCount = std::stoi(line);
+        lineCounter++;
+        continue;
+      } else if (lineCounter == 1) {
+        levelData->colCount = std::stoi(line);
 
-//   std::string line;
-//   std::fstream *myfile = ResourceManager::getInstance().openFile(
-//       resourceFilename, std::ios_base::in);
-//   if (myfile->is_open()) {
-//     int lineCounter = 0;
-//     int colCounter = 0;
-//     std::string x;
-//     std::string y;
-//     while (std::getline(*myfile, line)) {
-//       if (lineCounter == 0) {
-//         levelData->rowCount = std::stoi(line);
-//         lineCounter++;
-//         continue;
-//       } else if (lineCounter == 1) {
-//         levelData->colCount = std::stoi(line);
+        // // specify the default value to fill the vector elements
+        // levelData->levelGrid.resize(
+        //     levelData->rowCount,
+        //     std::vector<TdLevelItem>(levelData->colCount,
+        //     TdLevelItem::NONE));
+        // ;
 
-//         // // specify the default value to fill the vector elements
-//         // levelData->levelGrid.resize(
-//         //     levelData->rowCount,
-//         //     std::vector<TdLevelItem>(levelData->colCount,
-//         //     TdLevelItem::NONE));
-//         // ;
+        lineCounter++;
+        continue;
+      } else if (lineCounter == 2) {
+        x = line;
+        lineCounter++;
+        continue;
+      } else if (lineCounter == 3) {
+        y = line;
+        lineCounter++;
+        continue;
+      }
 
-//         lineCounter++;
-//         continue;
-//       } else if (lineCounter == 2) {
-//         x = line;
-//         lineCounter++;
-//         continue;
-//       } else if (lineCounter == 3) {
-//         y = line;
-//         lineCounter++;
-//         continue;
-//       }
+      levelData->blockSize = Vector2D<int>(std::stoi(x), std::stoi(y));
 
-//       levelData->blockSize = Vector2D<int>(std::stoi(x), std::stoi(y));
+      // Traverse the string
+      colCounter = 0;
 
-//       // Traverse the string
-//       colCounter = 0;
+      for (auto &ch : line) {
+        auto data = TdBlockData();
+        if (ch == '.') {
+          data.block_Type = TdBlockType::Plain;
+          data.blockNumber = ch;
+          levelData->levelGrid[lineCounter - 4][colCounter] =
+              TdLevelItem::PLAINBLOCK;
+        } else if (ch == '1') {
+          data.block_Type = TdBlockType::Path;
+          data.blockNumber = ch;
+          levelData->levelGrid[lineCounter - 4][colCounter] =
+              TdLevelItem::PATHBLOCK;
+        }
+        levelData->blocks.push_back(data);
+        colCounter++;
+      }
 
-//       for (auto &ch : line) {
-//         auto data = TdBlockData();
-//         if (ch == '1') {
-//           data.block_Type = TdBlockType::PlainBlock;
-//           data.blockNumber = ch;
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//               TdLevelItem::TILE1;
-//         } else if (ch == '2') {
-//           data.block_Type = TdBlockType::PlainBlock;
-//           data.blockNumber = ch;
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//               TdLevelItem::TILE2;
-//         } else if (ch == '3') {
-//           data.block_Type = TdBlockType::PlainBlock;
-//           data.blockNumber = ch;
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//               TdLevelItem::TILE3;
-//         } else if (ch == 'X') {
-//           data.block_Type = TdBlockType::PlainBlock;
-//           data.blockNumber = ch;
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//               TdLevelItem::TILEX;
-//         } else if (ch == 'Y') {
-//           data.block_Type = TdBlockType::PlainBlock;
-//           data.blockNumber = ch;
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//               TdLevelItem::TILEY;
-//         } else if (ch == 'Z') {
-//           data.block_Type = TdBlockType::PlainBlock;
-//           data.blockNumber = ch;
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//               TdLevelItem::TILEZ;
-//         } else if (ch == '.') {
-//           data.block_Type = TdBlockType::NoBlock;
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//               TdLevelItem::NOBLOCK;
-//         } else if (ch == 'W') {
-//           data.block_Type = TdBlockType::Wall;
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//           TdLevelItem::WALL;
-//         } else if (ch == 'K') {
-//           data.block_Type = TdBlockType::Key;
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//           TdLevelItem::KEY;
-//         } else if (ch == 'E') {
-//           // Enemy start position
-//           data.block_Type = TdBlockType::NoBlock;
-//           levelData->enemyStartPositions.push_back(
-//               Vector2D<int>(colCounter, lineCounter - 4));
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//               TdLevelItem::PATROLZOMBIE;
-//         } else if (ch == 'F') {
-//           // Follower Enemy start position
-//           data.block_Type = TdBlockType::NoBlock;
-//           levelData->followerEnemyStartPositions.push_back(
-//               Vector2D<int>(colCounter, lineCounter - 4));
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//               TdLevelItem::FOLLOWZOMBIE;
-//         } else if (ch == 'P') {
-//           data.block_Type = TdBlockType::NoBlock;
-//           levelData->playerStartPosition =
-//               Vector2D<int>(colCounter, lineCounter - 4);
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//           TdLevelItem::JACK;
-//         } else if (ch == 'G') {
-//           data.block_Type = TdBlockType::Exit;
-//           levelData->levelEndPosition =
-//               Vector2D<int>(colCounter, lineCounter - 4);
-//           levelData->levelGrid[lineCounter - 4][colCounter] =
-//           TdLevelItem::EXIT;
-//         }
+      lineCounter++;
+    }
 
-//         levelData->blocks.push_back(data);
-//         colCounter++;
-//       }
-
-//       lineCounter++;
-//     }
-
-//     ResourceManager::getInstance().closeFile(resourceFilename);
-//   } else {
-//     std::cout << "Unable to open level file at " << resourceFilename
-//               << std::endl;
-//   }
-// }
+    ResourceManager::getInstance().closeFile(resourceFilename);
+  } else {
+    std::cout << "Unable to open level file at " << resourceFilename
+              << std::endl;
+  }
+}
 
 // void updateCurrentLevel(TdLevelData *levelData, Vector2D<int> gridPosition,
 //                         TdLevelItem item) {
