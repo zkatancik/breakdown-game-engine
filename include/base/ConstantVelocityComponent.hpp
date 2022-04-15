33a814d444cd7d0aff663e6a93cbff332c6af70b
Marcin Kierzenka <33709159+mkierzenka@users.noreply.github.com>
2022-04-15 10:54:57 -0400

    Added some missing includes to ConstantVelocityComponent

--- include/base/ConstantVelocityComponent.hpp
+++ include/base/ConstantVelocityComponent.hpp
@@ -1,18 +1,27 @@
 #ifndef CONSTANT_VELOCITY_COMPONENT_HPP
 #define CONSTANT_VELOCITY_COMPONENT_HPP
-//! \brief Component for keeping the GameObject's velocity constant across frames
-class JmpConstantVelocityComponent: public GenericComponent {
+
+#include <memory>
+
+#include "GameObject.hpp"
+#include "GenericComponent.hpp"
+#include "PhysicsComponent.hpp"
+
+//! \brief Component for keeping the GameObject's velocity constant across
+//! frames
+class JmpConstantVelocityComponent : public GenericComponent {
  public:
-  JmpConstantVelocityComponent(GameObject & gameObject, float Vx, float Vy) : GenericComponent(gameObject), mVx(Vx), mVy(Vy) {}
+  JmpConstantVelocityComponent(GameObject &gameObject, float Vx, float Vy)
+      : GenericComponent(gameObject), mVx(Vx), mVy(Vy) {}
 
   void update(Level &level) override {
     std::shared_ptr<PhysicsComponent> pc = getGameObject().physicsComponent();
     pc->setVx(mVx);
     pc->setVy(mVy);
   }
+
  private:
   float mVx;
   float mVy;
-
 };
 #endif
