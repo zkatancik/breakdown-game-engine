af0c8f1648de0db3cb6337dc99253978b1e247da
Marcin Kierzenka <33709159+mkierzenka@users.noreply.github.com>
2022-04-23 13:46:50 -0400

    Added some docs and reformatted HealthComponent

--- include/base/HealthComponent.hpp
+++ include/base/HealthComponent.hpp
@@ -12,9 +12,16 @@ class HealthComponent: public GenericComponent {
  public:
   HealthComponent(GameObject& gameObject, int health);
 
-  void update(Level & level) override; //!< Update the object.
+  /**
+   * @brief Updates this health component, does either the onDeath callback or
+   * the onUpdate callback as applicable. Also removes the game object
+   * automatically once health drops to zero or below.
+   *
+   * @param level level information
+   */
+  void update(Level &level) override;
 
-  void collision(Level & level, std::shared_ptr<GameObject> obj) override;
+  void collision(Level &level, std::shared_ptr<GameObject> obj) override;
 
   void setCallbackAtDeath(const std::function<void(void)> &callbackAtDeath);
   void setCallbackAtUpdate(const std::function<void(int)> &callbackAtUpdate);
